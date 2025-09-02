1e916a6972a946408e5d896104bd034df8db4bd4
Casper Helliwell <68733738+cnmhqwerty@users.noreply.github.com>
2025-09-02 15:40:41 +0100

    Prefabs and comments

--- Runtime/Input/InteractionObject.cs
+++ Runtime/Input/InteractionObject.cs
@@ -4,6 +4,10 @@ namespace MMUCAVE
 {
     public abstract class InteractionObject : MonoBehaviour
     {
+        /// <summary>
+        /// All Interaction Objects must implement OnTouch as its primary function
+        /// Requires a collider on the object to call OnTouch
+        /// </summary>
         public abstract void OnTouch();
     }
 }
